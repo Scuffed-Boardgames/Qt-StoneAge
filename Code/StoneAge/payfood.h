ff35dbcac31ee86da80a063aa654df2f8ca3f446
Bernd U <berndu2002@gmail.com>
2021-05-04 11:19:07 +0200

    Really added pay for food

--- Code/StoneAge/payfood.h
+++ Code/StoneAge/payfood.h
@@ -2,11 +2,28 @@
 #define PAYFOOD_H
 
 #include <QDialog>
+#include "player.h"
 
+namespace Ui {
+class PayFood;
+}
 class PayFood : public QDialog
 {
+    Q_OBJECT
 public:
-    PayFood();
+    PayFood(std::shared_ptr<Player> player, int amount);
+    ~PayFood();
+private slots:
+    void on_acceptButton_clicked();
+    void on_scoreButton_clicked();
+
+private:
+    int totalSet();
+    void removeResources();
+    QString colourToString(Colour colour);
+    std::shared_ptr<Player> m_player;
+    int m_amount;
+    Ui::PayFood* ui;
 };
 
 #endif // PAYFOOD_H
