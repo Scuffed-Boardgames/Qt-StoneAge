347bf865872a3b58bb7351b1d9b21cd17132475e
Bernd U <berndu2002@gmail.com>
2021-05-14 10:56:02 +0200

    Completed payciv

--- Code/StoneAge/payciv.cpp
+++ Code/StoneAge/payciv.cpp
@@ -1,14 +1,50 @@
 #include "payciv.h"
 #include "ui_payciv.h"
 
-PayCiv::PayCiv(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::PayCiv)
-{
+PayCiv::PayCiv(std::shared_ptr<Player> player, std::shared_ptr<Civilisation> civCard)
+    : QDialog(nullptr), m_player{player}, m_civ{civCard}, m_amount{m_civ->getCost()}, m_hasPayed{false}, ui(new Ui::PayCiv){
+
     ui->setupUi(this);
+    this->setWindowTitle(player->getString());
+    ui->payLabel->setText("You have to pay " + QString::number(m_amount) + " items for this card");
+    ui->spinBoxWood->setMaximum(player->getResource(Resource::wood));
+    ui->spinBoxClay->setMaximum(player->getResource(Resource::clay));
+    ui->spinBoxStone->setMaximum(player->getResource(Resource::stone));
+    ui->spinBoxGold->setMaximum(player->getResource(Resource::gold));
 }
 
-PayCiv::~PayCiv()
-{
+PayCiv::~PayCiv(){
     delete ui;
+}
+
+void PayCiv::on_cancelButton_clicked(){
+    m_player = nullptr;
+    this->close();
+}
+
+int PayCiv::totalSet(){
+    int total = 0;
+    total += ui->spinBoxWood->value();
+    total += ui->spinBoxClay->value();
+    total += ui->spinBoxStone->value();
+    total += ui->spinBoxGold->value();
+    return total;
+}
+
+void PayCiv::removeResources(){
+    m_player->addResource(Resource::wood, -ui->spinBoxWood->value());
+    m_player->addResource(Resource::clay, -ui->spinBoxClay->value());
+    m_player->addResource(Resource::stone, -ui->spinBoxStone->value());
+    m_player->addResource(Resource::gold, -ui->spinBoxGold->value());
+}
+
+void PayCiv::on_acceptButton_clicked(){
+    int total = totalSet();
+    if(total != m_amount){
+        return;
+    }
+    removeResources();
+    m_player = nullptr;
+    m_hasPayed = true;
+    this->close();
 }
--- Code/StoneAge/payciv.h
+++ Code/StoneAge/payciv.h
@@ -2,6 +2,8 @@
 #define PAYCIV_H
 
 #include <QDialog>
+#include "player.h"
+#include "civilisation.h"
 
 namespace Ui {
 class PayCiv;
@@ -12,10 +14,20 @@ class PayCiv : public QDialog
     Q_OBJECT
 
 public:
-    explicit PayCiv(QWidget *parent = nullptr);
+    explicit PayCiv(std::shared_ptr<Player> player, std::shared_ptr<Civilisation> civCard);
     ~PayCiv();
 
+private slots:
+    void on_acceptButton_clicked();
+    void on_cancelButton_clicked();
+
 private:
+    int totalSet();
+    void removeResources();
+    std::shared_ptr<Player> m_player;
+    std::shared_ptr<Civilisation> m_civ;
+    int m_amount;
+    bool m_hasPayed;
     Ui::PayCiv *ui;
 };
 
